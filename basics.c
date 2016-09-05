3b456cadecda4bab148800526e6d10ab2522f9de
tom <tom.putzeys@gmail.com>
2016-09-05 22:16:16 +0200

    Added comments

--- basics.c
+++ basics.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello Tass!\n");
-	printf(argv[0]);
-	printf("\n");
 
+	//Basic hello world
+	printf("Hello Tass!\n");
+
+	//Program name
+	printf("%s\n", argv[0]);
+
+	//Print all args
 	for (int i = 0; i<argc; i++)
 	{
 		printf("%s\n",argv[i]);
 	}
 
+	//Print all args except program name
 	for (int i = 1; i<argc; i++)
 	{
 		printf("%s\n",argv[i]);
 	}
 
+	//Print all args in reverse order, except program name
 	for (int i = argc-1; i>0; i--)
 	{
 		printf("%s\n",argv[i]);
